5f681ed3e501bc380d53b54de3d91351cc9e46c1
seungwook <rlatmd0829@naver.com>
2021-10-26 22:54:42 +0900

    2021.10.26 백준 문제풀이

---
+++ src/October/boj_2231_분해합.java
@@ -0,0 +1,29 @@
+package October;
+
+import java.util.Scanner;
+
+public class boj_2231_분해합 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            if(re(i) == n){
+                System.out.println(i);
+                return;
+            }
+        }
+        System.out.println(0);
+
+
+    }
+    public static int re(int i){
+        int sum = i;
+        while (i > 0){
+            sum += i % 10;
+            i = i / 10;
+        }
+        return sum;
+
+    }
+}
