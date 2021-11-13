92045b3baad91a4d7e769c1a3dd236f75444ebdb
gimseung-ug <rlatmd0829@naver.com>
2021-11-13 20:18:04 +0900

    21.11.13 코테 문제풀이

---
+++ src/November/pro_2019카카오인턴십_불량사용자.java
@@ -0,0 +1,14 @@
+package November;
+
+public class pro_2019카카오인턴십_불량사용자 {
+    public int solution(String[] user_id, String[] banned_id) {
+        int answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        String[] user_id = {"frodo", "fradi", "crodo", "abc123", "frodoc"};
+        String[] banned_id = {"fr*d*", "abc1**"};
+        //solution();
+    }
+}
---
+++ src/November/test.java
@@ -0,0 +1,20 @@
+package November;
+
+public class test {
+    public static void main(String[] args) {
+        String[] arr = {"1", "234", "56789"};
+        boolean[] time = {true, false, true};
+
+    }
+//     0 0 1 0 0
+//     0 2 3 4 0
+//     5 6 7 8 9  {"1", "234", "56789"}
+//
+//     0 0 5 0 0
+//     0 7 6 2 0
+//     9 8 4 3 1  {"5", "762", "98431"}
+//
+//        9
+//      4 8 7
+//     1 3 2 6 5 {"9", "487", "13265"}
+}
