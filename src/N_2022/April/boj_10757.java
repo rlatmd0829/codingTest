1ffa5c22f8f884cc08980337001d164ba51f9e1f
seungwook <rlatmd0829@naver.com>
2022-04-12 22:30:37 +0900

    feat : boj_10757 문제풀이

---
+++ src/N_2022/April/boj_10757.java
@@ -0,0 +1,14 @@
+package N_2022.April;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class boj_10757 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        BigInteger A = sc.nextBigInteger();
+        BigInteger B = sc.nextBigInteger();
+
+        System.out.println(A.add(B));
+    }
+}
