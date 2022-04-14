fb531dc2e45b2d42f557be425b034b16e26bec3b
seungwook <rlatmd0829@naver.com>
2022-04-14 21:24:08 +0900

    feat : 백준문제풀이

---
+++ src/N_2022/April/boj_10926.java
@@ -0,0 +1,11 @@
+package N_2022.April;
+
+import java.util.Scanner;
+
+public class boj_10926 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        System.out.println(s + "??!");
+    }
+}
