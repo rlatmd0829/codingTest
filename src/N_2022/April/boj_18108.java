2cbc1da0f39579b921d0c96c4e744ab4cf461c85
seungwook <rlatmd0829@naver.com>
2022-04-20 23:22:11 +0900

    feat : 백준 문제풀이

---
+++ src/N_2022/April/boj_18108.java
@@ -0,0 +1,12 @@
+package N_2022.April;
+
+import java.util.Scanner;
+
+public class boj_18108 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int y = sc.nextInt();
+        System.out.println(y - 543);
+
+    }
+}
