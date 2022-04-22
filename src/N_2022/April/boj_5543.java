dd58cfb7a6bd8a09fcfc8ca3af09c8057c5d95e0
gimseung-ug <rlatmd0829@naver.com>
2022-04-22 16:01:04 +0900

    feat : 백준 문제풀이

---
+++ src/N_2022/April/boj_5543.java
@@ -0,0 +1,33 @@
+package N_2022.April;
+
+import java.util.Scanner;
+
+public class boj_5543 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] burgers = new int[3];
+        int[] beverages = new int[2];
+        int minBuger = Integer.MAX_VALUE;
+        int minBeverage = Integer.MAX_VALUE;
+
+        for (int i = 0; i < 3; i++) {
+            burgers[i] = sc.nextInt();
+            if (minBuger > burgers[i]) {
+                minBuger = burgers[i];
+            }
+        }
+
+        for (int i = 0; i < 2; i++) {
+            beverages[i] = sc.nextInt();
+            if (minBeverage > beverages[i])
+                minBeverage = beverages[i];
+        }
+
+        System.out.println(minBeverage + minBuger - 50);
+
+
+
+
+
+    }
+}
