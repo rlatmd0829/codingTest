89f849ac3e8749d8036971eb8d43882dfd5575e9
gimseung-ug <rlatmd0829@naver.com>
2022-05-06 11:51:21 +0900

    feat : boj_1550 문제풀이

---
+++ src/N_2022/May/boj_1550.java
@@ -0,0 +1,10 @@
+package N_2022.May;
+
+import java.util.Scanner;
+
+public class boj_1550 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(Integer.parseInt(sc.next(), 16));
+    }
+}
