171bc423617cf36b11f21c64384f25112a729dea
gimseung-ug <rlatmd0829@naver.com>
2022-05-09 23:08:49 +0900

    feat : boj_10699 문제풀이

---
+++ src/N_2022/May/boj_10699.java
@@ -0,0 +1,12 @@
+package N_2022.May;
+
+
+import java.time.LocalDate;
+
+
+public class boj_10699 {
+    public static void main(String[] args) {
+        LocalDate now = LocalDate.now();
+        System.out.println(now);
+    }
+}
