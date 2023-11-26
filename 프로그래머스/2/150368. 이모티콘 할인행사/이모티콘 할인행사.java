5bc19b2bbc29baa89eef3e063403a2a6e4d166e5
Seunguk <rlatmd0829@naver.com>
2023-11-26 14:42:34 +0900

    [level 2] Title: 이모티콘 할인행사, Time: 2.96 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.java
+++ 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.java
@@ -3,71 +3,73 @@ import java.util.Comparator;
 import java.util.List;
 
 class Solution {
-    public static final List<Money> list = new ArrayList<>();
+    public static final List<Result> resultList = new ArrayList<>();
+	public static int[] discountRates = {10, 20, 30, 40};
+	public static int[][] users = new int[][]{};
+	public static int[] emoticons = new int[]{};
+	public static int[][] discountAmount = new int[][]{};
 
 	public int[] solution(int[][] users, int[] emoticons) {
-		int[] answer = {};
-		int[] discountRates = {10, 20, 30, 40};
-		int[][] discountAmount = new int[emoticons.length][4];
-		int total = 0;
+		Solution.users = users;
+		Solution.emoticons = emoticons;
+		discountAmount = new int[emoticons.length][4];
 
-		// 모든 이모티콘에 대해 10, 20, 30, 40 할인율 적용된 값 미리 계산하기
 		for (int i = 0; i < emoticons.length; i++) {
-			total += emoticons[i];
-
 			for (int j = 0; j < discountRates.length; j++) {
 				int discount = (int)(emoticons[i] * discountRates[j] * 0.01);
 				discountAmount[i][j] = (emoticons[i] - discount);
 			}
 		}
 
-		permutation(discountRates, new int[emoticons.length], 0, emoticons.length, users, discountAmount);
+		permutation(new int[emoticons.length], 0);
 
-		list.sort(Comparator.comparingInt((Money o) -> o.cnt)
+		resultList.sort(Comparator.comparingInt((Result o) -> o.cnt)
 			.thenComparingInt(o -> o.amount)
 			.reversed());
 
-		return list.get(0).get();
+		return resultList.get(0).get();
 	}
 
-	public void permutation(
-		int[] discountRates, int[] out, int depth, int r, int[][] users, int[][] discountAmount
-	) {
-		if (depth == r) {
-			calculate(out, users, discountAmount);
+	public void permutation(int[] permutationArray, int depth) {
+		if (depth == emoticons.length) {
+			calculate(permutationArray);
 			return;
 		}
-		for (int i = 0; i < discountRates.length; i++) {
-			out[depth] = discountRates[i];
-			permutation(discountRates, out, depth + 1, r, users, discountAmount);
+		for (int discountRate : discountRates) {
+			permutationArray[depth] = discountRate;
+			permutation(permutationArray, depth + 1);
 		}
 	}
 
-	public void calculate(int[] arr, int[][] users, int[][] discountAmount) {
+	public void calculate(int[] permutationArray) {
 		int cnt = 0;
 		int amount = 0;
-		for (int i = 0; i < users.length; i++) {
+		for (int[] user : users) {
+			int usersRate = user[0];
+			int usersTotalAmount = user[1];
+
 			int total = 0;
-			for (int j = 0; j < arr.length; j++) {
-				if (users[i][0] <= arr[j]) {
-					int index = arr[j] / 10 - 1;
+			for (int j = 0; j < permutationArray.length; j++) {
+				if (usersRate <= permutationArray[j]) {
+					int index = permutationArray[j] / 10 - 1;
 					total += discountAmount[j][index];
 				}
 			}
-			if (total >= users[i][1]) {
+
+			if (usersTotalAmount <= total) {
 				cnt++;
 			} else {
 				amount += total;
 			}
 		}
-		list.add(new Money(cnt, amount));
+		resultList.add(new Result(cnt, amount));
 	}
 
-	public class Money {
+	public class Result {
 		public int cnt;
 		public int amount;
 
-		public Money(int cnt, int amount) {
+		public Result(int cnt, int amount) {
 			this.cnt = cnt;
 			this.amount = amount;
 		}
