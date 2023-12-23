244d61fb214edc0b3ddd3f12afeff689e5d37924
rlatmd0829 <rlatmd0829@naver.com>
2023-12-23 20:31:22 +0900

    feat : 코테 문제풀이

---
+++ src/N_2023/inflearn/section2/소수_에라토스테네스체_5_1.java
@@ -0,0 +1,23 @@
+package N_2023.inflearn.section2;
+
+import java.util.Scanner;
+
+public class 소수_에라토스테네스체_5_1 {
+	public static void main(String[] args){
+		Scanner in=new Scanner(System.in);
+		int n = in.nextInt();
+		int answer = 0;
+		int[] array = new int[n+1];
+
+		for (int i = 2; i < n; i++) {
+			if (array[i] == 0) {
+				answer++;
+				for (int j = i; j < n; j+=i) {
+					array[j] = 1;
+				}
+			}
+
+		}
+		System.out.println(answer);
+	}
+}
