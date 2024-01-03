d2b51d000a50e66bf8f00a024b35b05e5d7525c8
rlatmd0829 <rlatmd0829@naver.com>
2024-01-03 17:39:16 +0900

    feat : 코테 문제풀이

---
+++ src/inflearn/section2/보이는_학생_2.java
@@ -0,0 +1,21 @@
+package inflearn.section2;
+
+import java.util.Scanner;
+
+public class 보이는_학생_2 {
+	public static void main(String[] args){
+		Scanner in=new Scanner(System.in);
+		int input1 = in.nextInt();
+		int answer = 0;
+
+		int temp = 0;
+		for (int i = 0; i < input1; i++) {
+			int input2 = in.nextInt();
+			if (temp < input2) {
+				temp = input2;
+				answer++;
+			}
+		}
+		System.out.println(answer);
+	}
+}
