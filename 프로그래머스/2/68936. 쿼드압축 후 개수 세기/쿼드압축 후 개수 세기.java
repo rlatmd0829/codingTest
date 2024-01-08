f6c5945ade77a2fad5a134041ca70918fd29803a
Seungwook <rlatmd0829@naver.com>
2024-01-08 19:36:56 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 1077.64 ms, Memory: 408 MB -BaekjoonHub

--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
@@ -16,13 +16,6 @@ class Solution {
 		map.put(1, 0);
 		boolean[][] visited = new boolean[arr.length][arr.length];
 
-		// while (length > 1) {
-		// 	boolean oneQuadrant = bfs(arr, 0, 0, length/2, length/2);
-		// 	boolean twoQuadrant = bfs(arr, 0, length/2, length/2, length);
-		// 	boolean threeQuadrant = bfs(arr, length/2, 0, length, length/2);
-		// 	boolean fourQuadrant = bfs(arr, length/2, length/2, length, length);
-		// 	length = length/2;
-		// }
 		recursive(arr, 0, 0, length, length, length, visited);
 
 		answer[0] = map.get(0);
@@ -33,43 +26,23 @@ class Solution {
 
 	public void recursive(int[][] arr, int x1, int y1, int x2, int y2, int length, boolean[][] visited) {
 		if (length == 1) {
-			if (!visited[x1][y1] && arr[x1][y1] == 1) {
+			if (arr[x1][y1] == 1) {
 				map.put(1, map.get(1) + 1);
-			} else if (!visited[x1][y1] && arr[x1][y1] == 0) {
+			} else if (arr[x1][y1] == 0) {
 				map.put(0, map.get(0) + 1);
 			}
-			// if (!visited[x1+1][y1] && arr[x1+1][y1] == 1) {
-			// 	map.put(1, map.get(1) + 1);
-			// } else if (!visited[x1+1][y1] && arr[x1+1][y1] == 0) {
-			// 	map.put(0, map.get(0) + 1);
-			// }
-			// if (!visited[x1][y1+1] && arr[x1][y1+1] == 1) {
-			// 	map.put(1, map.get(1) + 1);
-			// } else if (!visited[x1][y1+1] && arr[x1][y1+1] == 0){
-			// 	map.put(0, map.get(0) + 1);
-			// }
-			// if (!visited[x1+1][y1+1] && arr[x1+1][y1+1] == 1) {
-			// 	map.put(1, map.get(1) + 1);
-			// } else if (!visited[x1+1][y1+1] && arr[x1+1][y1+1] == 0){
-			// 	map.put(0, map.get(0) + 1);
-			// }
-			visited[x1][y1] = true;
-			// visited[x1+1][y1] = true;
-			// visited[x1][y1+1] = true;
-			// visited[x1+1][y1+1] = true;
+			// visited[x1][y1] = true;
 			return;
 		}
 
 		boolean check = bfs(arr, x1, y1, x2, y2);
 		if (check) {
 			map.put(arr[x1][y1], map.get(arr[x1][y1]) + 1);
-
-			for (int i = x1; i < x2; i++) {
-				for (int j = y1; j < y2; j++) {
-					visited[i][j] = true;
-				}
-			}
-
+			// for (int i = x1; i < x2; i++) {
+			// 	for (int j = y1; j < y2; j++) {
+			// 		visited[i][j] = true;
+			// 	}
+			// }
 			return;
 		}
 
