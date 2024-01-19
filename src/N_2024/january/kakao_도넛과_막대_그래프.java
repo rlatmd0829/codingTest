14512f0c979a515968e4f9b9dfff97770bf01e26
rlatmd0829 <rlatmd0829@naver.com>
2024-01-19 22:26:18 +0900

    feat : 코테 문제풀이

--- src/N_2024/january/kakao_도넛과_막대_그래프.java
+++ src/N_2024/january/kakao_도넛과_막대_그래프.java
@@ -1,4 +1,116 @@
 package N_2024.january;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 public class kakao_도넛과_막대_그래프 {
+	static ArrayList<ArrayList<Integer>> graph;
+	static boolean[] visited = new boolean[1000000];
+
+	public static void main(String[] args) {
+		int[][] edges = {{4, 11}, {1, 12}, {8, 3}, {12, 7}, {4, 2}, {7, 11}, {4, 8}, {9, 6}, {10, 11}, {6, 10}, {3, 5}, {11, 1}, {5, 3}, {11, 9}, {
+		3, 8}};
+		List<Integer> list = solution(edges);
+		System.out.println(Arrays.toString(list.toArray()));
+	}
+
+	public static List<Integer> solution(int[][] edges) {
+		List<Integer> answer = new ArrayList<>();
+		graph=new ArrayList<>();
+
+		for (int i = 0; i < 1000000; i++) {
+			graph.add(new ArrayList<>());
+		}
+
+		for (int[] edge : edges) {
+			graph.get(edge[0]).add(edge[1]);
+		}
+
+		int maxx = 0;
+		int index = 0;
+
+		int[] count = new int[1000000];
+
+		// edge[0]이 가장 많은 노드를 정점으로 취급하자
+		for (int[] edge : edges) {
+			count[edge[0]]++;
+			if (count[edge[0]] > maxx) {
+				maxx = count[edge[0]];
+				index = edge[0];
+			}
+		}
+
+		List<Integer> start = new ArrayList<>();
+
+		// 정점에서 연결된 값들을 구한다
+		for (int[] edge : edges) {
+			if (index == edge[0]) {
+				start.add(edge[1]);
+			}
+		}
+
+		int donut = 0;
+		int stick = 0;
+		int eight = 0;
+
+		for (Integer i : start) {
+			Node node = bfs(i);
+
+			// 도넛 모양 그래프 : 크기가 n이면 n개의 정점과 n개의 간선
+			// 막대 모양 그래프 : 크기가 n이면 n개의 정점과 n-1개의 간선
+			// 8자 모양 그래프 : 크기가 n이면 2n+1개의 정점과 2n+2개의 간선이 있다.
+
+			if (node.vertex == node.line) {
+				donut++;
+			}
+			if (node.vertex > node.line) {
+				stick++;
+			}
+			if (node.vertex < node.line) {
+				eight++;
+			}
+		}
+		answer.add(index);
+		answer.add(donut);
+		answer.add(stick);
+		answer.add(eight);
+
+		return answer;
+	}
+
+	public static Node bfs(int V) {
+		Queue<Integer> queue = new LinkedList<>();
+		int vertex = 0;
+		int line = 0;
+
+		queue.add(V);
+		visited[V] = true;
+
+		while (!queue.isEmpty()) {
+			int temp = queue.poll();
+			vertex++;
+
+			for (Integer i : graph.get(temp)) {
+				line++;
+				if (!visited[i]) {
+					queue.add(i);
+					visited[i] = true;
+				}
+			}
+		}
+		return new Node(vertex, line);
+	}
+
+	public static class Node {
+		int vertex;
+		int line;
+
+		public Node(int vertex, int line) {
+			this.vertex = vertex;
+			this.line = line;
+		}
+	}
 }
