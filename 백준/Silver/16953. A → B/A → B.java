b9a13dcb194880da1fb3a38f20bae1859a04b9c6
Seungwook <rlatmd0829@naver.com>
2024-05-16 22:28:57 +0900

    [Silver II] Title: A → B, Time: 308 ms, Memory: 22836 KB -BaekjoonHub

---
+++ 백준/Silver/16953. A → B/A → B.java
@@ -0,0 +1,32 @@
+
+import java.util.Scanner;
+
+public class Main {
+    static int result = 0;
+    static boolean check = false;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long A = sc.nextInt();
+        long B = sc.nextInt();
+        recursive(A, B, 0);
+        if (check) {
+            System.out.println(result+1);
+        } else {
+            System.out.println(-1);
+        }
+    }
+
+    private static void recursive(long A, long B, int cnt) {
+        if (A > B) {
+            return;
+        }
+        if (A == B) {
+            check = true;
+            result = cnt;
+            return;
+        }
+
+        recursive(A * 2, B, cnt+1);
+        recursive(Long.parseLong(A + "1"), B, cnt+1);
+    }
+}
