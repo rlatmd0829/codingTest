b4ccdd4c9e212368232f915025e78127b07131e7
Seungwook <rlatmd0829@naver.com>
2024-05-26 20:01:15 +0900

    [Silver IV] Title: 기타줄, Time: 224 ms, Memory: 17812 KB -BaekjoonHub

---
+++ 백준/Silver/1049. 기타줄/기타줄.java
@@ -0,0 +1,34 @@
+
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+
+        int a = N / 6;
+        int b = N % 6;
+        int min = Integer.MAX_VALUE;
+
+        int[] P = new int[M];
+        int[] EA = new int[M];
+
+        for (int i = 0; i < M; i++) {
+            P[i] = sc.nextInt();
+            EA[i] = sc.nextInt();
+        }
+        Arrays.sort(P);
+        Arrays.sort(EA);
+
+        if (P[0] < EA[0] * b) {
+            min = Math.min(min, a * P[0] + P[0]);
+        } else {
+            min = Math.min(min, a * P[0] + EA[0] * b);
+        }
+        min = Math.min(min, P[0] * (a+1));
+        System.out.println(Math.min(min, EA[0] * N));
+    }
+}
