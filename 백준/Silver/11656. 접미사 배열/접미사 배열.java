8e1ae2014aaebe8a3bbaf4349edc87520ffa8813
Seungwook <rlatmd0829@naver.com>
2024-07-24 21:40:50 +0900

    [Silver IV] Title: 접미사 배열, Time: 344 ms, Memory: 20976 KB -BaekjoonHub

---
+++ 백준/Silver/11656. 접미사 배열/접미사 배열.java
@@ -0,0 +1,27 @@
+
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        char[] c = s.toCharArray();
+        for (int i = 0; i < c.length; i++) {
+            StringBuilder temp = new StringBuilder();
+            for (int j = i; j < c.length; j++) {
+                temp.append(c[j]);
+            }
+            list.add(temp.toString());
+        }
+        Collections.sort(list);
+        for (String s1 : list) {
+            System.out.println(s1);
+        }
+    }
+}
